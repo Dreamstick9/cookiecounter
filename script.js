9f922f8919c522cc23dd1418ebefdf888501ca3b
Dreamstick <dreamstick909@gmail.com>
2026-01-07 11:48:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const countEL = document.getElementById("count");
+const inc = document.getElementById("inc");
+const dec = document.getElementById("dec");
+const res = document.getElementById("op");
+const rad = document.getElementById("random");
+const heh = document.getElementById("ko");
+inc.addEventListener("click", function(){
+    countEL.innerText++;
+})
+dec.addEventListener("click", function(){
+    countEL.innerText--;
+})
+res.addEventListener("click", function(){
+    countEL.innerText = 0;
+})
+rad.addEventListener("click", function(){
+    countEL.innerText = parseInt(Math.random() * 1000);
+})
+heh.addEventListener("click", function(){
+    countEL.innerText = 67;
+})
